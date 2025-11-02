3d00f5d03aafc2198655e343d64c7f8d516127b9
HuyHoangTran <23020536@vnu.edu.vn>
2025-11-02 21:47:30 +0700

    test: use H2 profile and fix reserved table names (users/roles) to stabilize CI

--- complete/src/main/java/com/example/securingweb/model/Role.java
+++ complete/src/main/java/com/example/securingweb/model/Role.java
@@ -3,7 +3,7 @@ package com.example.securingweb.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "role")
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
